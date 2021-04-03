40f1589c89b9324095512c178fc6a0774d934988
hanker99 <yakin@hotmail.ca>
2021-04-03 18:06:42 -0400

    add intelijProjects project

---
+++ main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        if (a > 0 && b <= 0 && c <= 0) {
+            System.out.print(true);
+        } else if (a <= 0 && b > 0 && c <= 0) {
+            System.out.print(true);
+        } else if (a <= 0 && b <= 0 && c > 0) {
+            System.out.print(true);
+        } else {
+            System.out.print(false);
+        }
+    }
+}
